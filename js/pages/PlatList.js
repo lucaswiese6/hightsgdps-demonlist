1fc739d229da9b68b5c219c6801d4b8050021b9c
lucaswiese6 <lucaswiese9@gmail.com>
2026-05-30 14:22:37 +0200

    Update PlatList.js

--- js/pages/PlatList.js
+++ js/pages/PlatList.js
@@ -44,7 +44,7 @@ export default {
                     <ul class="stats">
                         <li>
                             <div class="type-title-sm">Points when completed</div>
-                            <p>{{ 0 || score(selected + 1, 100, level.percentToQualify) }}</p>
+                            <p>0</p>
                         </li>
                         <li>
                             <div class="type-title-sm">ID</div>
